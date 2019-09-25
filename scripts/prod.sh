1d3e87a6716ac0271d1a08243be0dbfc592cdf2d
Vitor Falcao <vitorfhcosta@gmail.com>
2019-09-25 15:17:13 -0300

    Finishes prod script

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -16,14 +16,15 @@ echo Building $image:$tag
 
 # build the application
 step='build app'
+docker-compose up -d webapp
 hash=`docker ps --format "{{.ID}} {{.Image}}" |\
     grep webapp_webapp |\
     cut -f1 -d ' '`
-docker exec -it $hash quasar build || eval $failed
+#docker exec -it $hash quasar build || eval $failed
 
 # build docker image
 step='build image'
-docker build -t "$image:$tag" Dockerfile.prod || eval $failed
+docker build -t "$image:$tag" -f Dockerfile.prod . || eval $failed
 
 # publish image
 docker push "$image:$tag" || eval $failed
