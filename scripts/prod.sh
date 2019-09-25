a6683fc226827f19ca36140302d78e7c09ab678e
Vitor Falcao <vitorfhcosta@gmail.com>
2019-09-24 21:02:44 -0300

    Creates prod.sh

---
+++ scripts/prod.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# fail function
+fail() {
+    echo Step $step returned $code
+}
+
+code=0
+image='vitorfhc/irrifront'
+tag='hml'
+failed="code=$?; fail; exit $code"
+
+[ $PROD ] && tag='prod'
+
+echo Building $image:$tag
+
+# build the application
+step='build app'
+hash=`docker ps --format "{{.ID}} {{.Image}}" |\
+    grep webapp_webapp |\
+    cut -f1 -d ' '`
+docker exec -it $hash quasar build || eval $failed
+
+# build docker image
+step='build image'
+docker build -t "$image:$tag" Dockerfile.prod || eval $failed
+
+# publish image
+docker push "$image:$tag" || eval $failed
