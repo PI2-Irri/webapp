135a8ed8d3be42f0c5d53afbb10be2850d4aa022
Vitor Falcao <vitorfhcosta@gmail.com>
2019-09-28 10:42:42 -0300

    TopBar layout

---
+++ src/layouts/TopBar.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  q-layout
+    q-page-container
+      div.flex.bar
+        img(src="statics/images/ativo5.png")
+      router-view
+</template>
+
+<script>
+export default {
+  name: 'TopBarLayout'
+}
+</script>
+
+<style lang="stylus" scoped>
+.bar
+  background-color #0a5959
+  width 100%
+  height 50px
+  justify-content center
+  align-content center
+  img
+    max-width 100%
+    max-height 90%
+    overflow hidden
+</style>
