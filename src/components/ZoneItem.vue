7e515338a5ffa076d86b324d1d9f62c12ec1ed2e
guilacerda <guiglacerda@gmail.com>
2019-10-14 15:32:44 -0300

    Fixes routes conflict

---
+++ src/components/ZoneItem.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  div.zone-item.flex
+    span {{ name }}
+    q-icon(name="keyboard_arrow_right" size="xl")
+</template>
+
+<script>
+export default {
+  name: 'ZonesItemComponent',
+  props: {
+    id: {
+      // TODO: Change this type to the database type
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.zone-item
+  justify-content space-between
+  padding 25px 20px
+  align-items center
+</style>
---
+++ src/pages/Zones.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  q-page(style="background-color: #EFEFEF")
+    div.flex.zones
+      span.zones_controller-name {{ getControllerName() }}
+
+      div.zones_menu
+        zone-item(:key="it" v-for="it in ['maisde60', 'oque', 'peixespo', 'maisde60', 'oque', 'peixespo', 'maisde60', 'oque', 'peixespo']" id="1" :name="it").zone-item
+
+</template>
+
+<script>
+import ZoneItem from 'components/ZoneItem.vue'
+
+export default {
+  name: 'ZonesPage',
+  components: {
+    'zone-item': ZoneItem
+  },
+  methods: {
+    getControllerName () {
+      return 'SouFoda'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.zone-item + .zone-item
+  border-top 4px solid #efefef
+
+.zones
+  width 100%
+  height 100%
+  justify-content center
+
+.zones_controller-name
+  font-size 30px
+  font-weight 400
+  color $grey-8
+  margin 25px 0
+
+.zones_menu
+  border-radius 8px
+  width 90%
+  max-height 60vh
+  overflow auto
+  background-color $grey-1
+</style>
