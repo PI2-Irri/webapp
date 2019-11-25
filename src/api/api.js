35d94924c766f7d8dff9c6b297f4b772c67ea460
guilacerda <guiglacerda@gmail.com>
2019-11-25 11:35:39 -0300

    Creates function to get schedules from API
    Co-authored-by: Guilherme Augusto <guilherme.francais@gmail.com>

--- src/api/api.js
+++ src/api/api.js
@@ -9,7 +9,8 @@ const apiEndpoints = {
   CONTROLLERS_INFO: 'controllers_info/',
   CONTROLLERS: 'controllers/',
   ZONES: 'zones/',
-  MEASUREMENTS: 'zones_info/'
+  MEASUREMENTS: 'zones_info/',
+  SCHEDULES: 'schedules_info/'
 }
 
 async function get (endpoint, param = {}, header = {}) {
@@ -109,11 +110,19 @@ async function getZonesInfo (param, owner) {
   return res.data
 }
 
+async function getSchedulesInfo (param, owner) {
+  const res = await get(apiEndpoints.SCHEDULES, { params: param }, generateHeader(owner))
+    .then((res) => res)
+
+  return res.data
+}
+
 export {
   makeLogin,
   makeSignUp,
   connectControllers,
   getControllersInfo,
   createZone,
-  getZonesInfo
+  getZonesInfo,
+  getSchedulesInfo
 }
